510541616b966a3652cd6116d574910f05f4fbbd
garretthilberling <ghilberling@knights.ucf.edu>
2022-07-06 19:25:05 -0400

    built out UI for profile page

--- app/src/main/java/com/hilberling/treblleapp/ui/screens/Profile.kt
+++ app/src/main/java/com/hilberling/treblleapp/ui/screens/Profile.kt
@@ -1,2 +1,60 @@
 package com.hilberling.treblleapp.ui.screens
 
+import android.widget.Button
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.hilberling.treblleapp.R
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Profile() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Card(modifier = Modifier
+            .fillMaxWidth()
+            .align(Alignment.Center)
+            .padding(horizontal = 32.dp),
+            shape= RoundedCornerShape(4), elevation = 6.dp) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(top = 16.dp)) {
+                    Image(painter = painterResource(id = R.drawable.ic_launcher_background),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(200.dp)
+                            .border(shape = CircleShape, width = 1.dp, color = Color.White)
+                            .clip(CircleShape)
+                            .background(color = Color.LightGray)
+                        )
+                    Text(text = "User Name", modifier = Modifier.padding(top = 10.dp))
+                    Button(onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp, vertical = 32.dp)
+                            .fillMaxWidth()
+                    ) {
+                        Text(text = "Edit Profile")
+                    }
+                }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfilePreview() {
+    Profile()
+}
